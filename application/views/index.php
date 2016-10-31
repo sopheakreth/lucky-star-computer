9ccaf2eb4f8145d80feafc2cb61b1ee97ae763df
sopheakreth <rethsopheak@gmail.com>
2016-10-31 12:53:46 +0700

    upload

---
+++ application/views/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home | Lucky Star Computer</title>
+
+<?php $this->load->view('layouts/header') ?>
+
+<?php $this->load->view('layouts/homeSlider') ?>
+
+
+<?php $this->load->view('layouts/footer') ?>
---
+++ application/views/layouts/homeSlider.php
@@ -0,0 +1,63 @@
+
+	<section id="slider"><!--slider-->
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div id="slider-carousel" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+							<li data-target="#slider-carousel" data-slide-to="1"></li>
+							<li data-target="#slider-carousel" data-slide-to="2"></li>
+						</ol>
+						
+						<div class="carousel-inner">
+							<div class="item active">
+								<div class="col-sm-6">
+									<h1>MacBook Pro</h1>
+									<h2>with Retina display</h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<button type="button" class="btn btn-default get">Get it now</button>
+								</div>
+								<div class="col-sm-6">
+									<img src="<?=base_url();?>public/images/home/macbookpro.jpg" class="girl img-responsive" alt="" />
+									<!-- <img src="images/home/pricing.png"  class="pricing" alt="" /> -->
+								</div>
+							</div>
+							<div class="item">
+								<div class="col-sm-6">
+									<h1>ASUS G752</h1>
+									<h2>Gaming and Design</h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<button type="button" class="btn btn-default get">Get it now</button>
+								</div>
+								<div class="col-sm-6">
+									<img src="<?=base_url();?>public/images/home/asus-g752.jpg" class="girl img-responsive" alt="" />
+								</div>
+							</div>
+							
+							<div class="item">
+								<div class="col-sm-6">
+									<h1>The new iMac</h1>
+									<h2>Performance and Design</h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<button type="button" class="btn btn-default get">Get it now</button>
+								</div>
+								<div class="col-sm-6">
+									<img src="<?=base_url();?>public/images/home/imac.png" class="girl img-responsive" alt="" />
+								</div>
+							</div>
+							
+						</div>
+						
+						<a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+							<i class="fa fa-angle-left"></i>
+						</a>
+						<a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+							<i class="fa fa-angle-right"></i>
+						</a>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</section><!--/slider-->
